f929a452dddceaa8b2b0f818aeb77a196ce44c24
Jalil Tahirov <jalil.tahirov@tenforce.com>
2022-07-22 10:18:34 +0400

    completed binary search

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -10,12 +10,49 @@ namespace BinarySearch
     {
         static void Main(string[] args)
         {
-            binary_search(new int[] {3,5,6,7}, 3);
+            var result = binary_search(new int[] {3,5,6,7}, 1);
+            Console.WriteLine(result);
+            Console.ReadKey();
         }
 
-        private static void binary_search(int[] list, int itemToFind)
+        private static int binary_search(int[] list, int itemToFind)
         {
+            /*
+               check if the item is equal to the mid element
+             
+               if it is more update low
+               if it is less update high 
+           */
+
+            int high = list.Length - 1;
+            int low = 0;
             
+
+            while (low <= high)
+            {
+                int mid = (high + low) / 2;
+                int guess = list[mid];
+                if (itemToFind == guess)
+                {
+                    return mid;
+                }
+
+                if (guess > itemToFind)
+                {
+                    high = mid - 1;
+                }
+
+                if (guess < itemToFind)
+                {
+                    low = mid + 1; 
+                }
+            }
+
+
+
+            return -888;
+
+
         }
     }
 }
